6af68e88d49dae965d328c88b7433623fdcc9b92
Evan Momen <evanmomen@gmail.com>
2025-02-23 03:10:33 -0800

    Add layout and component files

---
+++ src/components/Test.svelte
@@ -0,0 +1,15 @@
+<script>
+    let myvariable = $state(false);
+
+    function changeState() {
+        myvariable = !myvariable;
+    }
+</script>
+
+{#if myvariable}
+  <p>yes it's true</p>
+{:else}
+  <p>no it's false</p>
+{/if}
+
+<button onclick={changeState}>Click me</button>
